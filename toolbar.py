630ac6ad798783c2e0ba8b530cbc62fc03154d27
AhmedHamdi0 <ahmed88hamdi99@gmail.com>
2023-07-12 16:34:08 +0300

    Add toolbar icons and action
    Each action is associated with a relevant icon to enhance the visual representation of the toolbar to improve the user interface

--- toolbar.py
+++ toolbar.py
@@ -1,4 +1,5 @@
-from PySide2.QtWidgets import QToolBar
+from PySide2.QtGui import QIcon
+from PySide2.QtWidgets import QToolBar, QAction
 
 
 class Toolbar(QToolBar):
@@ -15,4 +16,23 @@ class Toolbar(QToolBar):
         self.init_toolbar()
 
     def init_toolbar(self):
-        print("Toolbar")
+        self.new_plot_action = QAction(QIcon("Assets/function.png"), "New Plot", self)
+        self.new_plot_action.setCheckable(True)
+
+        self.add_subplot_action = QAction(QIcon("Assets/blueprint--plus.png"), "Add Subplot", self)
+        self.add_subplot_action.setCheckable(True)
+
+        self.save_image_action = QAction(QIcon("Assets/document-image.png"), "Save Image", self)
+        self.save_image_action.setCheckable(True)
+
+        self.export_pdf_action = QAction(QIcon("Assets/blue-document-pdf-text.png"), "Export PDF", self)
+        self.export_pdf_action.setCheckable(True)
+
+        self.zoom_in_action = QAction(QIcon("Assets/magnifier-zoom-in.png"), "Zoom In", self)
+        self.zoom_in_action.setCheckable(True)
+
+        self.zoom_out_action = QAction(QIcon("Assets/magnifier-zoom-out.png"), "Zoom Out", self)
+        self.zoom_out_action.setCheckable(True)
+
+        self.exit_action = QAction(QIcon("Assets/cross-script.png"), "Exit App", self)
+        self.exit_action.setCheckable(True)
