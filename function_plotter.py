66931a4f1ce6edd617a781d75bbf36ae0113f649
AhmedHamdi0 <ahmed88hamdi99@gmail.com>
2023-07-12 16:54:56 +0300

    Implement function plotter UI
     This implementation sets up the foundation for capturing user input and triggering the plot action.

--- function_plotter.py
+++ function_plotter.py
@@ -1,4 +1,4 @@
-from PySide2.QtWidgets import QWidget
+from PySide2.QtWidgets import QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout, QHBoxLayout
 
 
 class FunctionPlotter(QWidget):
@@ -12,4 +12,29 @@ class FunctionPlotter(QWidget):
         self.init_plotter()
 
     def init_plotter(self):
-        print("Function Plotter")
+        plotter_layout = QVBoxLayout()
+
+        function_layout = QHBoxLayout()
+        function_label = QLabel("Enter function (e.g., 5*x^3 + 2*x):", self)
+        self.function_input = QLineEdit(self)
+
+        function_layout.addWidget(function_label)
+        function_layout.addWidget(self.function_input)
+
+        value_x_layout = QHBoxLayout()
+        min_label = QLabel("Enter min value of x:", self)
+        self.min_input = QLineEdit(self)
+        max_label = QLabel("Enter max value of x:", self)
+        self.max_input = QLineEdit(self)
+
+        value_x_layout.addWidget(min_label)
+        value_x_layout.addWidget(self.min_input)
+        value_x_layout.addWidget(max_label)
+        value_x_layout.addWidget(self.max_input)
+
+        self.plot_button = QPushButton("Plot", self)
+
+        plotter_layout.addLayout(function_layout)
+        plotter_layout.addLayout(value_x_layout)
+        plotter_layout.addWidget(self.plot_button)
+
